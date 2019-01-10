201b6b691e69dcf45a306ca6accac371e6a05538
luhongru <973339083@qq.com>
2019-01-10 22:35:23 +0800

    SSMdemo done lhr

---
+++ src/main/java/com/cat/service/user/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cat.service.user.impl;
+
+import com.cat.dao.UserMapper;
+import com.cat.domain.User;
+import com.cat.service.user.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+    @Resource
+    private UserMapper userDao;
+    @Override
+    public User getUser(Integer uid) {
+       return userDao.selectByPrimaryKey(uid);
+    }
+}
